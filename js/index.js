183dbc6bfb591018b301f536a51a1976c841f46e
keithliam <keithliamm@gmail.com>
2018-05-03 16:59:30 +0800

    Revert to map fucntions

--- js/index.js
+++ js/index.js
@@ -173,14 +173,14 @@ myApp.controller('thisController', ['$scope', '$timeout', function($scope, $time
     	var values = []
     	var bestMoves = []
     	var value
-    	nextMoves.some(function(move, i){
+    	nextMoves.map((move, i) => {
     		value = $scope.AImin($scope.AInextState($scope.cells, move[0], move[1], 2))
     		values.push(value)
     		if(value > max){
     			max = value
     		}
     	})
-    	values.some(function(value, i){
+    	values.map((value, i) => {
     		if(value === max){
     			bestMoves.push(i)
     		}
