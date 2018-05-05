d68cb0e2b0760d994b2bb8a0c4733464464c6fac
keithliam <keithliamm@gmail.com>
2018-05-05 21:57:03 +0800

    Fix AI double move

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 Array.prototype.clone = function(arr){
-	var arr = arguments[0] == null? [] : arguments[0] 
-	for(var p = 0; p < this.length; p++){
-		if(this[p] instanceof Array){
-			var sub = [] ;
-			this[p].clone(sub) ;
-			arr.push(sub.slice(0));
-		} else {
-			arr.push(this[p])
-		}
-	}
-	return arr
+    var arr = arguments[0] == null? [] : arguments[0] 
+    for(var p = 0; p < this.length; p++){
+        if(this[p] instanceof Array){
+            var sub = [] ;
+            this[p].clone(sub) ;
+            arr.push(sub.slice(0));
+        } else {
+            arr.push(this[p])
+        }
+    }
+    return arr
 }
 
 var myApp = angular.module('myApp', ['ngAnimate'])
@@ -35,29 +35,29 @@ myApp.controller('thisController', ['$scope', '$timeout', function($scope, $time
     $scope.colorize = 'colorize1'
     $scope.AIchoosing = false
     $scope.changePlayers = function(){
-    	$scope.is1Player = !$scope.is1Player
-	    $scope.cells = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-	    $scope.turn = 1
-	    $scope.firstTurn = 1
-	    $scope.win = false
-	    $scope.draw = false
-	    $scope.lock = false
-	    $scope.cell00 = false
-	    $scope.cell01 = false
-	    $scope.cell02 = false
-	    $scope.cell10 = false
-	    $scope.cell11 = false
-	    $scope.cell12 = false
-	    $scope.cell20 = false
-	    $scope.cell21 = false
-	    $scope.cell22 = false
-	    $scope.AIchoosing = false
+        $scope.is1Player = !$scope.is1Player
+        $scope.cells = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        $scope.turn = 1
+        $scope.firstTurn = 1
+        $scope.win = false
+        $scope.draw = false
+        $scope.lock = false
+        $scope.cell00 = false
+        $scope.cell01 = false
+        $scope.cell02 = false
+        $scope.cell10 = false
+        $scope.cell11 = false
+        $scope.cell12 = false
+        $scope.cell20 = false
+        $scope.cell21 = false
+        $scope.cell22 = false
+        $scope.AIchoosing = false
     }
     $scope.checkWin = function(isAI){
         const cells = $scope.cells
         $scope.win = true
         $scope.lock = isAI? false : true
-		$scope.turn = $scope.turn === 1? 2 : 1
+        $scope.turn = $scope.turn === 1? 2 : 1
         if(cells[0][0] && cells[0][0] === cells[0][1] && cells[0][0] === cells[0][2]){
             $scope.cell00 = true
             $scope.cell01 = true
@@ -91,39 +91,39 @@ myApp.controller('thisController', ['$scope', '$timeout', function($scope, $time
             $scope.cell11 = true
             $scope.cell20 = true
         } else {
-	        const stringified = cells.toString()
-	        if(stringified.indexOf('0') < 0){
-	            $scope.draw = true
-	            $scope.lock = isAI? false : true
-	            $scope.turn = $scope.turn === 1? 2 : 1
-	        } else {
-	            $scope.win = false
-	            $scope.lock = false
-	            $scope.turn = $scope.turn === 1? 2 : 1
-	        }
+            const stringified = cells.toString()
+            if(stringified.indexOf('0') < 0){
+                $scope.win = false
+                $scope.draw = true
+                $scope.lock = isAI? false : true
+                $scope.turn = $scope.turn === 1? 2 : 1
+            } else {
+                $scope.win = false
+                $scope.lock = false
+                $scope.turn = $scope.turn === 1? 2 : 1
+            }
         }
     }
     $scope.move = function(row, col){
-    	const none = !$scope.cells[row][col]
+        const none = !$scope.cells[row][col]
         if(!$scope.win && !$scope.draw && none && !$scope.AIchoosing){
-            if($scope.is1Player){
-            	$scope.turn = 2
-	        	$scope.AIchoosing = true
-	            $scope.cells[row][col] = 1
-	            $scope.checkWin(false)
-	        	$timeout(function(){
-	        		if($scope.is1Player && !$scope.win && !$scope.draw){
-			            $scope.turn = 1
-		        		$scope.AImakeMove()
-			            $scope.checkWin(true)
-		        		$scope.AIchoosing = false
-		        	}
-	        	}, 1000)
-	        } else {
-	            $scope.cells[row][col] = $scope.turn
-	            $scope.turn = $scope.turn === 1? 2 : 1
-	            $scope.checkWin(false)
-	        }
+            if($scope.is1Player && $scope.turn === 1){
+                $scope.turn = 2
+                $scope.cells[row][col] = 1
+                $scope.checkWin(false)
+                if(!$scope.win && !$scope.draw){
+                    $scope.AIchoosing = true
+                    $scope.AImakeMove(function(){
+                        $scope.turn = 1
+                        $scope.checkWin(true)
+                        $scope.AIchoosing = false
+                    })
+                }
+            } else if(!$scope.is1Player) {
+                $scope.cells[row][col] = $scope.turn
+                $scope.turn = $scope.turn === 1? 2 : 1
+                $scope.checkWin(false)
+            }
         }
     }
     $scope.refresh = function(){
@@ -135,15 +135,12 @@ myApp.controller('thisController', ['$scope', '$timeout', function($scope, $time
             $scope.cells = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
             $scope.firstTurn = $scope.firstTurn === 1? 2 : 1;
             $scope.turn = $scope.firstTurn
-            if($scope.is1Player && $scope.firstTurn === 2 && !$scope.win && !$scope.draw){
-            	$scope.AIchoosing = true
-	        	$timeout(function(){
-	        		if($scope.is1Player){
-			            $scope.turn = 1
-		        		$scope.AImakeMove()
-		        		$scope.AIchoosing = false
-		        	}
-	        	}, 1000)
+            if($scope.is1Player && $scope.firstTurn === 2 && !$scope.win && !$scope.draw && !$scope.AIchoosing){
+                $scope.AIchoosing = true
+                $scope.AImakeMove(function(){
+                    $scope.AIchoosing = false
+                    $scope.turn = 1
+                })
             }
             $scope.cell00 = false
             $scope.cell01 = false
@@ -160,11 +157,7 @@ myApp.controller('thisController', ['$scope', '$timeout', function($scope, $time
         }
     }
 
-    $scope.AImakeMove = function(){
-	    const coordinates = $scope.AIgetNextMove()
-	    $scope.cells[coordinates[0]][coordinates[1]] = 2
-    }
-    $scope.AIgetNextMove = function(){
+    $scope.AImakeMove = function(callback){
         const nextMoves = $scope.AInextMoves($scope.cells)
         var max = -999
         var values = []
@@ -179,41 +172,45 @@ myApp.controller('thisController', ['$scope', '$timeout', function($scope, $time
             if(value === max) bestMoves.push(i)
         })
         const move = Math.floor(Math.random() * bestMoves.length)
-        return nextMoves[bestMoves[move]]
+        $timeout(function(){
+            const coordinates = nextMoves[bestMoves[move]]
+            $scope.cells[coordinates[0]][coordinates[1]] = 2
+            callback()
+        }, 1000)
     }
-	$scope.AImin = function(state, alpha){
-		var won = $scope.AIisTerminal(state)
-		if(won === -1){
-			const nextMoves = $scope.AInextMoves(state)
-	    	var min = 999
-	    	var value
-	    	for(var i = 0; i < nextMoves.length; i++){
-	    		value = $scope.AImax($scope.AInextState(state, nextMoves[i], 1), min)
+    $scope.AImin = function(state, alpha){
+        var won = $scope.AIisTerminal(state)
+        if(won === -1){
+            const nextMoves = $scope.AInextMoves(state)
+            var min = 999
+            var value
+            for(var i = 0; i < nextMoves.length; i++){
+                value = $scope.AImax($scope.AInextState(state, nextMoves[i], 1), min)
                 if(value < alpha) return value
                 if(value < min) min = value
-	    	}
-	    	return min
-	    } else {
-	    	return won
-	    }
-	}
+            }
+            return min
+        } else {
+            return won
+        }
+    }
     $scope.AImax = function(state, beta){
-		var lost = $scope.AIisTerminal(state)
-		if(lost === -1){
-			const nextMoves = $scope.AInextMoves(state)
-	    	var max = -999
-	    	var value
-	    	for(var i = 0; i < nextMoves.length; i++){
-	    		value = $scope.AImin($scope.AInextState(state, nextMoves[i], 2), max)
+        var lost = $scope.AIisTerminal(state)
+        if(lost === -1){
+            const nextMoves = $scope.AInextMoves(state)
+            var max = -999
+            var value
+            for(var i = 0; i < nextMoves.length; i++){
+                value = $scope.AImin($scope.AInextState(state, nextMoves[i], 2), max)
                 if(value > beta) return value
-	    		if(value > max) max = value
-	    	}
-	    	return max
-	    } else if(lost === 1){
-	    	return -1
-	    } else {
-	    	return 0
-	    }
+                if(value > max) max = value
+            }
+            return max
+        } else if(lost === 1){
+            return -1
+        } else {
+            return 0
+        }
     }
     $scope.AIisTerminal = function(state){
         if(
